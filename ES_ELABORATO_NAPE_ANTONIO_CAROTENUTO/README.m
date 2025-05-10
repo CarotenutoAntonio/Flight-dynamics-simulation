9c5297841487ac5971763fe3104a9baeac60ebdf
root <root@DESKTOP-49AOR9I.>
2025-05-10 12:34:00 +0200

    NewCommit

---
+++ ES_ELABORATO_NAPE_ANTONIO_CAROTENUTO/README.m
@@ -0,0 +1,14 @@
+%% Matlab 2017a+ required
+
+%% Step 0.
+% Execute script: setup_prerequisites.m
+
+%% Step 1.
+% execute live script: setup_nps.mlx
+
+%% Step 2.
+% Execute the Simulink model: Nonlinear_Performance_Simulation_1.slx
+
+%% Step 3.
+% execute live script: analyze_results.mlx
+
